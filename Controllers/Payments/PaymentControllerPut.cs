1ad5ebda5d2fa0a35f29a065028d3cf4765d7e39
kevin alvarez diaz <kevinadozanam23@gmail.com>
2024-09-02 17:41:48 -0500

    feat: the method put was added to PaymentController

--- Controllers/Payments/PaymentControllerPut.cs
+++ Controllers/Payments/PaymentControllerPut.cs
@@ -1,12 +1,49 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestAdminV2.Models;
 
-namespace RestAdminV2.Controllers.Payments
+namespace RestAdmin.Controllers
 {
-    public class PaymentControllerPut
+    public partial class PaymentController
     {
-        
+        // PUT: api/payment/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdatePayment(int id, [FromBody] Payment payment)
+        {
+            if (id != payment.IdPayment)
+            {
+                return BadRequest("Payment ID mismatch.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Entry(payment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PaymentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool PaymentExists(int id)
+        {
+            return _context.Payments.Any(e => e.IdPayment == id);
+        }
     }
 }
