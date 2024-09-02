4919c127de5b75e31696797ebbe7e3fa918e387f
KevinAlvarezD <kevinadozanam23@gmail.com>
2024-09-02 12:27:19 -0500

    feat: The program was created with de connection to database

--- Program.cs
+++ Program.cs
@@ -1,9 +1,31 @@
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+using RestAdminV2.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+Env.Load();
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbPort = Environment.GetEnvironmentVariable("DB_PORT");
+var dbDatabaseName = Environment.GetEnvironmentVariable("DB_DATABASE");
+var dbUser = Environment.GetEnvironmentVariable("DB_USERNAME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
 
-// Add services to the container.
-
+var mySqlConnection = $"server={dbHost};port={dbPort};database={dbDatabaseName};uid={dbUser};password={dbPassword}";
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(mySqlConnection,ServerVersion.Parse("8.0.20-mysql")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+// Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -15,11 +37,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("AllowAllOrigins");
 
 app.UseHttpsRedirection();
-
+app.UseWelcomePage();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
