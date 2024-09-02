28c95e664998f73ed47a202fa5e253e21b221ac2
kevin alvarez diaz <kevinadozanam23@gmail.com>
2024-09-02 17:57:09 -0500

    feat: The method delete was added to ProductController

--- Controllers/Products/ProductControllerDelete.cs
+++ Controllers/Products/ProductControllerDelete.cs
@@ -1,12 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestAdminV2.Models;
 
-namespace RestAdminV2.Controllers.Products
+namespace RestAdmin.Controllers
 {
-    public class ProductControllerDelete
+    public partial class ProductController
     {
-        
+        // DELETE: api/products/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
