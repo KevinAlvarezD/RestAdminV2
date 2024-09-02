5731a2e4bb4caa5b946bca13e2a85e62541c2908
kevin alvarez diaz <kevinadozanam23@gmail.com>
2024-09-02 17:39:38 -0500

    feat: The method post was added to PaymentController

--- Controllers/Payments/PaymentControllerPost.cs
+++ Controllers/Payments/PaymentControllerPost.cs
@@ -1,12 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestAdminV2.Models;
 
-namespace RestAdminV2.Controllers.Payments
+namespace RestAdmin.Controllers
 {
-    public class PaymentControllerPost
+    public partial class PaymentController
     {
-        
+        // POST: api/payment
+        [HttpPost]
+        public async Task<ActionResult<Payment>> CreatePayment([FromBody] Payment payment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Payments.Add(payment);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetPayment), new { id = payment.IdPayment }, payment);
+        }
     }
 }
