e4244e2c29c447ffc31b6cab997a8c14472bf5fe
CODEALEJO <salomee579@gmail.comgit config --global user.email salomee579@gmail.comgit config --global user.email salomee579@gmail.com>
2024-09-02 12:37:48 -0500

    properties are added to Employee

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,12 +1,27 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace RestAdminV2.Models
+namespace RestAdmin.Models;
+
+
+[Table("employees")]
+public class Employee
 {
-    public class Employee
-    {
-        
-    }
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+    [Column("name")]
+    [MaxLength(255, ErrorMessage = "The Name field must be at most {1} characters.")]
+    public string? Name { get; set; }
+    [Column("role")]
+    [MaxLength(255, ErrorMessage = "The Role field must be at most {1} characters.")]
+    public string? Role { get; set; }
+    [Column("schedule")]
+    [DataType(DataType.Date)]
+    public DateTime Schedule { get; set;}
+
 }
