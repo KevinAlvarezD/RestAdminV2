1f56225fdaba25617921cbcf09c2140c850d6131
kevin alvarez diaz <kevinadozanam23@gmail.com>
2024-09-02 17:56:17 -0500

    feat: the method put was added to productController

--- Controllers/Products/ProductControllerPut.cs
+++ Controllers/Products/ProductControllerPut.cs
@@ -1,12 +1,49 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestAdminV2.Models;
 
-namespace RestAdminV2.Controllers.Products
+namespace RestAdmin.Controllers
 {
-    public class ProductControllerPut
+    public partial class ProductController
     {
-        
+        // PUT: api/products/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(int id, [FromBody] Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest("Product ID mismatch.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.Id == id);
+        }
     }
 }
