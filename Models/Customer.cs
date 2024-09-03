0b1c12cb211d2da1028f42f1866cb91ee3482816
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2024-09-03 07:28:15 -0500

    feat: Add more dataanotation in models customer and administrator

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -15,19 +15,25 @@ public class Customer
     public int Id { get; set; }
 
     [Column("name")]
-    [MaxLength(50, ErrorMessage = "The field must be at most {1} character.")]
+    [MinLength(5, ErrorMessage = "The Name field must be at least {1} characters.")]
+    [MaxLength(90, ErrorMessage = "The Name field must be at most {1} characters.")]
     public string? Name { get; set; }
 
     [Column("address")]
-    [MaxLength(50, ErrorMessage = "The field must be at most {1} character.")]
+    [MinLength(5, ErrorMessage = "The address field must be at least {1} characters.")]
+    [MaxLength(50, ErrorMessage = "The address field must be at most {1} character.")]
     public string? Address { get; set; }
 
     [Column("phone_number")]
-    [MaxLength(50, ErrorMessage = "The field must be at most {1} character.")]
+    [MinLength(5, ErrorMessage = "The Telefono field be at least {1} characters.")]
+    [MaxLength(25, ErrorMessage = "The Telefono field  be at most {1} characters.")]
+    [Phone(ErrorMessage = "the phone format is not valid, you can use this example format if you want => '### ### ## ##'")]
     public string? PhoneNumber { get; set; }
 
     [Column("email")]
-    [MaxLength(50, ErrorMessage = "The field must be at most {1} character.")]
+    [EmailAddress(ErrorMessage = "The Email field is using an invalid format.")]
+    [MinLength(5, ErrorMessage = "The Email field must be at least {1} characters.")]
+    [MaxLength(255, ErrorMessage = "The Email field must be at most {1} characters.")]
     public string? Email { get; set; }
 
 }
