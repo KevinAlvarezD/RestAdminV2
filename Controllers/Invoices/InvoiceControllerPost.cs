56a4f5d075f17a10640bebc2987d1b08c3bf1e4f
KevinAlvarezD <kevinadozanam23@gmail.com>
2024-09-03 10:27:32 -0500

    feat: Add the logic for create PDF to invoice

--- Controllers/Invoices/InvoiceControllerPost.cs
+++ Controllers/Invoices/InvoiceControllerPost.cs
@@ -6,7 +6,6 @@ namespace RestAdmin.Controllers
     public partial class InvoiceController : ControllerBase
     {
       
-        // Constructor para inyección de dependencias
         public InvoiceController(ApplicationDbContext context, InvoiceService invoiceService)
         {
             _context = context;
@@ -24,22 +23,17 @@ namespace RestAdmin.Controllers
 
             try
             {
-                // Generar el PDF para la factura
                 byte[] pdfFile = _invoiceService.GenerateInvoicePdf(invoice);
 
-                // Asignar el PDF al modelo de factura
                 invoice.PdfFile = pdfFile;
 
-                // Agregar la factura a la base de datos
                 _context.Invoices.Add(invoice);
                 await _context.SaveChangesAsync();
 
-                // Retornar la respuesta con la ubicación de la nueva factura
                 return CreatedAtAction(nameof(GetInvoice), new { id = invoice.IdInvoice }, invoice);
             }
             catch (Exception ex)
             {
-                // Manejo de errores
                 return StatusCode(500, $"Internal server error: {ex.Message}");
             }
         }
--- Program.cs
+++ Program.cs
@@ -2,8 +2,13 @@ using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 using RestAdminV2.Models;
 
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Cargar variables de entorno
 Env.Load();
+
 var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
 var dbPort = Environment.GetEnvironmentVariable("DB_PORT");
 var dbDatabaseName = Environment.GetEnvironmentVariable("DB_DATABASE");
@@ -11,9 +16,12 @@ var dbUser = Environment.GetEnvironmentVariable("DB_USERNAME");
 var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
 
 var mySqlConnection = $"server={dbHost};port={dbPort};database={dbDatabaseName};uid={dbUser};password={dbPassword}";
+
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseMySql(mySqlConnection,ServerVersion.Parse("8.0.20-mysql")));
+    options.UseMySql(mySqlConnection, ServerVersion.Parse("8.0.20-mysql")));
+
+builder.Services.AddScoped<InvoiceService>();
 
 builder.Services.AddCors(options =>
 {
@@ -25,22 +33,22 @@ builder.Services.AddCors(options =>
                   .AllowAnyHeader();
         });
 });
-// Add services to the container.
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.UseCors("AllowAllOrigins");
 
 app.UseHttpsRedirection();
-// app.UseWelcomePage();
 app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
