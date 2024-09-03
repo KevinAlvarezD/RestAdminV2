3ff9ed8bb88397bd4cc6d9933d4d043d9d08510f
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2024-09-02 19:01:05 -0500

    feat: Create OrderDetailsControllerPut

---
+++ Controllers/OrderDetails/OrderDetailsControllerPut.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestAdmin.Models;
+using RestAdminV2.Models;
+
+namespace RestAdmin.Controllers
+{
+    public partial class OrderDetailsController
+    {
+        // PUT: api/OrderDetails/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrderDetails(int id, OrderDetails orderDetails)
+        {
+            if (id != orderDetails.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(orderDetails).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderDetailsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool OrderDetailsExists(int id)
+        {
+            return _context.OrderDetails.Any(e => e.Id == id);
+        }
+    }
+}
