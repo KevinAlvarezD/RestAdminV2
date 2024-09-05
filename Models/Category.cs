07b894f688e83b874c7bfbb5b8a85c02ea9bfa91
CODEALEJO <salomee579@gmail.comgit config --global user.email salomee579@gmail.comgit config --global user.email salomee579@gmail.com>
2024-09-05 08:07:41 -0500

    feat: The data annotations are added to Category

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,10 +9,16 @@ namespace RestAdminV2.Models;
 [Table("Category")]
 public class Category
 {
-    
+    [Key]
+    [Column("id")]
     public int Id { get; set; }
+    
+    [Column("name")]
+    [MaxLength(50, ErrorMessage = "The name must be at most {1} characters.")]
+    [Required(ErrorMessage = "The name is required.")]
     public string Name { get; set; }
 
+    //Foreing Links
     [ForeignKey("IdProduct")]
     public virtual Product Product { get; set; }
 }
