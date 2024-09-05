35ea4f63e667310a8eddd384ffb705a7dfa0842f
CODEALEJO <salomee579@gmail.comgit config --global user.email salomee579@gmail.comgit config --global user.email salomee579@gmail.com>
2024-09-05 08:11:33 -0500

    feat: Controllers were added to category

---
+++ Controllers/Category/CategoryControllerGet.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestAdminV2.Controllers.Category
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryControllerGet : ControllerBase
+    {
+        
+    }
+}
