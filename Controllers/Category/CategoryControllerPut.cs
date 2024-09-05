b1c3e42d0d9c0176f8afa82878c608effacc31b5
CODEALEJO <salomee579@gmail.comgit config --global user.email salomee579@gmail.comgit config --global user.email salomee579@gmail.com>
2024-09-05 09:05:55 -0500

    feat: the put method is added to category

--- Controllers/Category/CategoryControllerPut.cs
+++ Controllers/Category/CategoryControllerPut.cs
@@ -1,15 +1,44 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestAdminV2.Models;
 
-namespace RestAdminV2.Controllers.Category
+namespace RestAdmin.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class CategoryControllerPut : ControllerBase
+   public partial class CategoryController
     {
-        
+        // PUT: api/Category/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategory(int id, Category category)
+        {
+            if (id != category.Id)
+            {
+                return BadRequest("Category ID mismatch.");
+            }
+
+            _context.Entry(category).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return _context.Categorys.Any(item => item.Id == id);
+        }
     }
 }
