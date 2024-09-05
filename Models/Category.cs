0c89e2b04b1a08e764f742375f79ecef9045c511
CODEALEJO <salomee579@gmail.comgit config --global user.email salomee579@gmail.comgit config --global user.email salomee579@gmail.com>
2024-09-05 08:02:19 -0500

    feat: Properties are added to category and the product foreign key is also added.

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,10 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RestAdminV2.Models;
+[Table("Category")]
 public class Category
 {
     
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    [ForeignKey("IdProduct")]
+    public virtual Product Product { get; set; }
 }
