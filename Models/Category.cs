7ea1d41558a077e98e138d8a4657dc653b06f45e
CODEALEJO <salomee579@gmail.comgit config --global user.email salomee579@gmail.comgit config --global user.email salomee579@gmail.com>
2024-09-05 07:49:59 -0500

    feat: DataNotations are added to the company model

--- Models/Category.cs
+++ Models/Category.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace RestAdminV2.Models
+namespace RestAdminV2.Models;
+public class Category
 {
-    public class Category
-    {
-        
-    }
+
 }
