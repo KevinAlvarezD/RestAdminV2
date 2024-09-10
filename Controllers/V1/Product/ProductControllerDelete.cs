54955a92538d747c7d7270c3d2ade857d97a7175
KevinAlvarezD <kevinadozanam23@gmail.com>
2024-09-10 10:16:40 -0500

    feat: Create productController

---
+++ Controllers/V1/Product/ProductControllerDelete.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestAdminV2.Controllers.V1.Product
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductControllerDelete : ControllerBase
+    {
+        
+    }
+}
