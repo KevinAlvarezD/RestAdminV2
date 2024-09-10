5c9092d7a6c245b8b72392d9f1342f2215f8bb14
CODEALEJO <salomee579@gmail.comgit config --global user.email salomee579@gmail.comgit config --global user.email salomee579@gmail.com>
2024-09-10 10:43:56 -0500

    feat: the data of the different types of users is completed

--- Seeders/UserSeeder.cs
+++ Seeders/UserSeeder.cs
@@ -12,6 +12,7 @@ namespace RestAdminV2.Seeders
         public static void Seed(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>().HasData(
+               
                 new User
                 {
                     Id = 172443449,
@@ -20,7 +21,7 @@ namespace RestAdminV2.Seeders
                     Password =  "riwi1234",
                     Phone = "3242144893",
                     Address = "Cra 50 40 90",
-                    Role = "Administrator"
+                    Role = "Mesero"
                 },
                 new User
                 {
@@ -30,26 +31,27 @@ namespace RestAdminV2.Seeders
                     Password =  "Susana1901",
                     Phone = "3004001077",
                     Address = "Cra 50a 36 90",
-                    Role = "Administrator"
+                    Role = "Administrador"
                 },
                 new User
                 {
-                    Id = 1724434496,
+                    Id = 1724434495,
                     Name = "Alejandro Castrillón",
                     Email = "alejomi192005@gmail.com",
                     Password =  "21354",
                     Phone = "333245884",
                     Address = "Cra 59a 66 57",
-                    Role = "Administrator"
+                    Role = "Cajero"
                 },
-                new User{
+                new User
+                {
                     Id = 1724434497,
                     Name = "Mickey Londoño",
                     Email = "Mickey@gmail.com",
                     Password =  "12345678",
                     Phone = "3123456789",
                     Address = "Cra 45 67 89",
-                    Role = "Administrator"
+                    Role = "Mesero"
                 },
                 new User
                 {   
@@ -59,7 +61,37 @@ namespace RestAdminV2.Seeders
                     Password =  "987654321",
                     Phone = "3132145678",
                     Address = "Cra 40 50 60",
-                    Role = "Administrator"
+                    Role = "Administrador"
+                },
+                new User
+                {
+                    Id = 1724434499,
+                    Name = "Laura Jimenez",
+                    Email = "laura.jimenez@restadmin.com",
+                    Password = "laura2024",
+                    Phone = "3221234567",
+                    Address = "Cra 55 33 44",
+                    Role = "Mesero"
+                },
+                new User
+                {
+                    Id = 1724434500,
+                    Name = "Carlos Mejia",
+                    Email = "carlos.mejia@restadmin.com",
+                    Password = "admin2024",
+                    Phone = "3209876543",
+                    Address = "Cra 60 35 78",
+                    Role = "Administrador"
+                },
+                new User
+                {
+                    Id = 1724434501,
+                    Name = "Diana Lopez",
+                    Email = "diana.lopez@restadmin.com",
+                    Password = "cashier2024",
+                    Phone = "3111239876",
+                    Address = "Cra 42 55 88",
+                    Role = "Cajero"
                 }
             );
         }    
