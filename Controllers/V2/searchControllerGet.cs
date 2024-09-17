21538b5fbe8825a298bcc4d0078ec01c03b9a9fd
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2024-09-17 10:31:21 -0500

    feat: Add method of find to controller search

--- Controllers/V2/searchControllerGet.cs
+++ Controllers/V2/searchControllerGet.cs
@@ -8,7 +8,7 @@ namespace RestAdminV2.Controllers
     public partial class ProductController : ControllerBase
     {
         // GET: api/products/salesByMonth?month=5
-        [HttpGet("salesByMonth")]
+         [HttpGet("salesByMonth")]
         [SwaggerOperation(
             Summary = "Get invoices by month",
             Description = "Retrieves all pre-invoices for a specific month. The month should be between 1 and 12."
@@ -17,23 +17,50 @@ namespace RestAdminV2.Controllers
         [SwaggerResponse(200, "Returns a list of pre-invoices for the specified month.")]
         [SwaggerResponse(400, "If the provided month is not between 1 and 12.")]
         [SwaggerResponse(404, "If no invoices are found for the specified month.")]
-        public async Task<ActionResult<IEnumerable<PreInvoice>>> SearchByMonth([FromQuery] int month)
+        public async Task<ActionResult<IEnumerable<Invoice>>> SearchByMonth([FromQuery] int month)
         {
             if (month < 1 || month > 12)
             {
                 return BadRequest("Invalid month.");
             }
 
-            var invoices = await _context.PreInvoices
-                .Where(p => p.DateInvoice.Month == month)
+            var invoices = await _context.Invoices
+            .Where(p => p.DateInvoice.Month == month)
+            .ToListAsync();
+
+            if (invoices == null || !invoices.Any())
+            {
+                return NotFound("No invoices have been recorded this month yet.");
+            }
+
+            return Ok(invoices);
+
+        }
+
+
+        // GET: api/invoices/salesByDay?day=15&month=9&year=2024
+        [HttpGet("salesByDay")]
+        public async Task<ActionResult<IEnumerable<Invoice>>> SearchByDay(int day, int month, int year)
+        {
+          
+            if (day < 1 || day > 31 || month < 1 || month > 12 || year < 2000 || year > DateTime.Now.Year)
+            {
+                return BadRequest("Invalid date parameters.");
+            }
+
+            var invoices = await _context.Invoices
+                .Where(p => p.DateInvoice.Day == day
+                            && p.DateInvoice.Month == month
+                            && p.DateInvoice.Year == year)
                 .ToListAsync();
 
             if (invoices == null || !invoices.Any())
             {
-                return NotFound("No invoices found for the specified month.");
+                return NotFound("No invoices have been recorded on this day.");
             }
 
             return Ok(invoices);
         }
+
     }
 }
