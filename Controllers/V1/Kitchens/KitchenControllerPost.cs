8d4912e9003157ef8e706aefc91a23388ada2c17
AlejandroLondonoValle <londonovalleluisalejandro@gmail.com>
2024-09-17 10:17:22 -0500

    feat: Create Method of deltete in kitchen controller

--- Controllers/V1/Kitchens/KitchenControllerPost.cs
+++ Controllers/V1/Kitchens/KitchenControllerPost.cs
@@ -9,37 +9,37 @@ namespace RestAdmin.Controllers;
 
 public partial class KitchenController
 {
-// POST: api/Kitchen/create-from-order/{orderId}
-[HttpPost("create-from-order/{orderId}")]
-public async Task<IActionResult> CreateOrderKitchenFromOrder(int orderId)
-{
-   
-    var order = await _context.Orders
-        .Include(o => o.OrderProducts)
-        .ThenInclude(op => op.Product)
-        .FirstOrDefaultAsync(o => o.Id == orderId);
-
-    if (order == null || !order.OrderProducts.Any())
+    // POST: api/Kitchen/create-from-order/{orderId}
+    [HttpPost("create-from-order/{orderId}")]
+    public async Task<IActionResult> CreateOrderKitchenFromOrder(int orderId)
     {
-        return NotFound("Order not found or no products associated with this order.");
-    }
 
-    
-    var kitchen = new Kitchen
-    {
-        OrderId = order.Id,
-        KitchenItems = order.OrderProducts.Select(op => new KitchenItem
+        var order = await _context.Orders
+            .Include(o => o.OrderProducts)
+            .ThenInclude(op => op.Product)
+            .FirstOrDefaultAsync(o => o.Id == orderId);
+
+        if (order == null || !order.OrderProducts.Any())
         {
-            ProductId = op.Product.Id,
-            Quantity = op.Quantity
-        }).ToList()
-    };
-
-    _context.Kitchens.Add(kitchen);
-    await _context.SaveChangesAsync();
+            return NotFound("Order not found or no products associated with this order.");
+        }
 
 
-    return CreatedAtAction(nameof(GetKitchen), new { id = kitchen.Id }, kitchen);
-}
+        var kitchen = new Kitchen
+        {
+            OrderId = order.Id,
+            KitchenItems = order.OrderProducts.Select(op => new KitchenItem
+            {
+                ProductId = op.Product.Id,
+                Quantity = op.Quantity
+            }).ToList()
+        };
+
+        _context.Kitchens.Add(kitchen);
+        await _context.SaveChangesAsync();
+
+
+        return CreatedAtAction(nameof(GetKitchen), new { id = kitchen.Id }, kitchen);
+    }
 
 }
